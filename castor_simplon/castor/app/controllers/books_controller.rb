67b523a3083487029d45c66e9f0e4b1c6470874c
helwina <morphee3301@hotmail.fr>
2016-04-26 23:09:04 +0200

    modification du css casino et creation de l exercice castor simplon

---
+++ castor_simplon/castor/app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+    def index
+        @books = Book.all
+    end
+    
+    def create
+        Book.create title: params[:title]   
+        redirect_to "/books"
+    end
+    
+    def show
+        @book = Book.find_by(params[:id])
+    end
+    
+    def update
+        Book.find_by(params[:id]).update title: params[:title]
+        redirect_to "/books/#{params[:id]}"
+    end
+    
+    def destroy  
+        Book.find_by(params[:id]).destroy
+        redirect_to "/books"
+    end
+
+
+end
---
+++ castor_simplon/castor/app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def a_propos
+  end
+end
